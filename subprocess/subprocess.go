0a46ff67f1c55fb6cba74cd38e9e251e4b45e29b
Daniel McCarney <daniel@binaryparadox.net>
2025-01-22 13:26:05 -0500

    implement support for KTS-IFC
    Based on the NIST ACVP specification:
    
      https://pages.nist.gov/ACVP/draft-hammett-acvp-kas-ifc.html
    
    Some notable limitations:
    
    * Only the KTS-OAEP-basic scheme is supported.
    * Only the rsakpg1-basic keygen mode is supported.
    * Only the AFT test type is supported (no partialVal function support).

--- subprocess/subprocess.go
+++ subprocess/subprocess.go
@@ -146,6 +146,7 @@ func NewWithIO(cmd *exec.Cmd, in io.WriteCloser, out io.ReadCloser) *Subprocess 
 		"PBKDF":             &pbkdf{},
 		"ML-KEM":            &mlkem{},
 		"kdf-components":    &ssh{},
+		"KTS-IFC":           &kts{map[string]bool{"SHA-1": true, "SHA2-224": true, "SHA2-256": true, "SHA2-384": true, "SHA2-512": true, "SHA2-512/224": true, "SHA2-512/256": true, "SHA3-224": true, "SHA3-256": true, "SHA3-384": true, "SHA3-512": true}},
 	}
 	m.primitives["ECDSA"] = &ecdsa{"ECDSA", map[string]bool{"P-224": true, "P-256": true, "P-384": true, "P-521": true}, m.primitives}
 	m.primitives["DetECDSA"] = &ecdsa{"DetECDSA", map[string]bool{"P-224": true, "P-256": true, "P-384": true, "P-521": true}, m.primitives}
